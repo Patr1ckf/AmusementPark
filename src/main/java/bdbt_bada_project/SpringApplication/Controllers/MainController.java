75fa54e396531a62bf97a3770a068d0833358a67
pfigiel <01178639@pw.edu.pl>
2024-01-22 18:53:22 +0100

    done css for admin and translated navbar into polish

--- src/main/java/bdbt_bada_project/SpringApplication/Controllers/MainController.java
+++ src/main/java/bdbt_bada_project/SpringApplication/Controllers/MainController.java
@@ -2,10 +2,16 @@ package bdbt_bada_project.SpringApplication.Controllers;
 import bdbt_bada_project.SpringApplication.Entities.Pracownicy;
 import bdbt_bada_project.SpringApplication.Entities.PracownicyDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -40,4 +46,12 @@ public class MainController {
         }
     }
 
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/index";
+    }
 }
