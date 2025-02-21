e096ba7b6f0d6e8f25651de3e3b99daf60e6d0e7
Patryk <patryk.figiel8@gmail.com>
2025-02-21 22:53:39 +0100

    Upgraded version of project

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/SEBDM_pro-0.0.1-SNAPSHOT.jar
--- src/main/java/SpringApplication/Application.java
+++ src/main/java/SpringApplication/Application.java
@@ -15,32 +15,15 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
-//	public DataSource dataSource() {
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setUrl("jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:5432/postgres");
-//		dataSource.setUsername("postgres.wxeseejwtrlkyitubugy");
-//		dataSource.setPassword("dRWMSfNPgGDv3lxk");
-//		dataSource.setDriverClassName("org.postgresql.Driver");
-//		return dataSource;
-//	}
-@Bean
-public DataSource dataSource() {
-	// Pobieramy zmienne środowiskowe z Heroku
-	DriverManagerDataSource dataSource = new DriverManagerDataSource();
-	String dbUrl = System.getenv("SUPABASE_URL");
-	String dbUsername = System.getenv("SUPABASE_DB_USERNAME");
-	String dbPassword = System.getenv("SUPABASE_DB_PASSWORD");
-
-	dataSource.setUrl(dbUrl);
-	dataSource.setUsername(dbUsername);
-	dataSource.setPassword(dbPassword);
-
-	return dataSource;
-}
-
-
-
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUrl("jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:5432/postgres");
+		dataSource.setUsername("postgres.wxeseejwtrlkyitubugy");
+		dataSource.setPassword("dRWMSfNPgGDv3lxk");
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		return dataSource;
+	}
 	@Bean
 	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
