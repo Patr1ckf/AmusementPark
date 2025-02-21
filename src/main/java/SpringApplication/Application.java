a77f0b5fa697eab5ac7e6c968cf8f977b05ef91f
Patryk <patryk.figiel8@gmail.com>
2025-02-21 21:36:14 +0100

    corrected database configuration

--- src/main/java/SpringApplication/Application.java
+++ src/main/java/SpringApplication/Application.java
@@ -15,32 +15,32 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-//	@Bean
-//	public DataSource dataSource() {
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setUrl("jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:5432/postgres");
-//		dataSource.setUsername("postgres.wxeseejwtrlkyitubugy");
-//		dataSource.setPassword("dRWMSfNPgGDv3lxk");
-//		dataSource.setDriverClassName("org.postgresql.Driver");
-//		return dataSource;
-//	}
-
 	@Bean
 	public DataSource dataSource() {
-		String databaseUrl = System.getenv("SUPABASE_DB_URL");
-		// Upewnij się, że masz ustawioną zmienną środowiskową na Heroku
-
-		// Sprawdzamy, czy URL zaczyna się od 'postgres://', jeśli tak, zamieniamy to na 'jdbc:postgresql://'
-		if (databaseUrl != null && databaseUrl.startsWith("postgres://")) {
-			databaseUrl = databaseUrl.replace("postgres://", "jdbc:postgresql://");
-		}
-
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUrl(databaseUrl);  // Ustawiamy URL bazy danych
-		dataSource.setDriverClassName("org.postgresql.Driver");  // Ustawiamy sterownik PostgreSQL
-
+		dataSource.setUrl("jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:5432/postgres");
+		dataSource.setUsername("postgres.wxeseejwtrlkyitubugy");
+		dataSource.setPassword("dRWMSfNPgGDv3lxk");
+		dataSource.setDriverClassName("org.postgresql.Driver");
 		return dataSource;
 	}
+
+//	@Bean
+//	public DataSource dataSource() {
+//		String databaseUrl = System.getenv("SUPABASE_DB_URL");
+//		// Upewnij się, że masz ustawioną zmienną środowiskową na Heroku
+//
+//		// Sprawdzamy, czy URL zaczyna się od 'postgres://', jeśli tak, zamieniamy to na 'jdbc:postgresql://'
+//		if (databaseUrl != null && databaseUrl.startsWith("postgres://")) {
+//			databaseUrl = databaseUrl.replace("postgres://", "jdbc:postgresql://");
+//		}
+//
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setUrl(databaseUrl);  // Ustawiamy URL bazy danych
+//		dataSource.setDriverClassName("org.postgresql.Driver");  // Ustawiamy sterownik PostgreSQL
+//
+//		return dataSource;
+//	}
 
 
 
